d751cd7978fc99c3b2f10e84c2f9b72c555f0930
Serafeim Papastefanos <spapas@gmail.com>
2020-05-08 12:55:45 +0300

    Fix project for wagtail 2.9

--- latest_changes/views.py
+++ latest_changes/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
-from wagtail.admin.views.reports import ReportView
+from wagtail.admin.views.reports import PageReportView
 from wagtail.admin.auth import permission_denied
 from wagtail.core.models import UserPagePermissionsProxy
 from wagtail.core.models import Page
 
 
-class LatestChangesView(ReportView):
+class LatestChangesView(PageReportView):
     template_name = "reports/latest_changes.html"
     title = "Latest changes"
     header_icon = "date"
 
     def get_queryset(self):
-
         self.queryset = Page.objects.exclude(last_published_at__isnull=True).order_by("-last_published_at")
         return super().get_queryset()
 
